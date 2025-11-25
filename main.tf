977bb5de00cb7f2f9d15013e0e1bb1aabacc82bc
ShashiAnandSharma <shashianand8651@gmail.com>
2025-11-25 22:12:27 +0530

    Remove Terraform required providers configuration
    Removed Terraform configuration block for required providers and version.

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-# Terraform configuration
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 # Define the provider (AWS)
 provider "aws" {
   region = "us-east-1"
